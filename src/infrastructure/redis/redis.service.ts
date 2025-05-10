89c364e2cea14c1cd2722d4017bff0a36c1a61cf
Masum-Osman <ogkmasum@gmail.com>
2025-05-10 11:23:19 +0600

    test(redis): get, set done. now have to add those on root module

--- src/infrastructure/redis/redis.service.ts
+++ src/infrastructure/redis/redis.service.ts
@@ -1,4 +1,5 @@
 import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import Redis from 'ioredis';
 
 @Injectable()
@@ -6,10 +7,27 @@ export class RedisService implements OnModuleInit {
     private readonly logger = new Logger(RedisService.name);
     private redisClient: Redis;
 
-    constructor(){}
+    constructor(
+        private readonly configService: ConfigService,
+    ) {
+        this.redisClient = new Redis({
+            host: this.configService.get('redis.host'),
+            port: this.configService.get('redis.port'),
+            password: this.configService.get('redis.password'),
+            db: this.configService.get('redis.db'),
+        });
+    }
+    
     onModuleInit() {    }
 
-    async set(){}
+    async set<T>(key: string, value: T, ttlInSec?: number): Promise<void> {
+        const ttl = ttlInSec || this.configService.get('redis.ttl');
+        await this.redisClient.set(key, JSON.stringify(value));
+    }
+    
 
-    async get(){}
+    async get<T>(key: string): Promise<T | null> {
+        const value = await this.redisClient.get(key);
+        return value ? JSON.parse(value) : null;
+      }
 }
