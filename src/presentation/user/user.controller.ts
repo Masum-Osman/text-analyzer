4ad01469c23450cdf79c03b95094a99607676a85
Masum-Osman <ogkmasum@gmail.com>
2025-05-11 16:29:47 +0600

    feat(api): addded signup api on user controlle

--- src/presentation/user/user.controller.ts
+++ src/presentation/user/user.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { UserService } from './user.service';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+    constructor(private readonly userService: UserService) {}
+  
+    @Post('signup')
+    async signup(@Body() signupDto: CreateUserDto) {
+      return this.userService.create(signupDto);
+    }
+  }
